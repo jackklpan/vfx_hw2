78c3e28cc0687ccf49372d82ef7ef2d2556b99a1
Jayis <jayis4176@gmail.com>
2014-04-26 10:21:21 +0800

    MSOP_done

--- MSOP_Matching.m
+++ MSOP_Matching.m
@@ -1,13 +1,17 @@
-function [ output_args ] = MSOP_Matching( a, b )
+function [ aFP_position, bFP_position ] = MSOP_Matching( a, b )
 % extract feature of a
 [aFP, aD, aD_vec, aC] = MSOP_Feature(a);
-aC_arr = [aC{:}]';
-
 % extract feature of b
 [bFP,bD,bD_vec, bC] = MSOP_Feature(b);
-bC_arr = [bC{:}]';
 
+%% parameter
+% outlier ratio
+outlier_ratio = 0.23;
+
+%% start matching
 % use HaarWT Coeficient find KNN
+aC_arr = [aC{:}]';
+bC_arr = [bC{:}]';
 [Approx_IDX, D] = knnsearch(aC_arr, bC_arr, 'K', 10, 'NSMethod', 'kdtree');
 
 % use Descriptor find BestNN from KNN
@@ -23,24 +27,34 @@ end
 % err_outlier = average of err_2ndNN 
 err_outlier = sum(sec_err(:)) / length(sec_err);
 
-matched = (first_err < 0.65 * err_outlier);
+matched = (first_err < outlier_ratio * err_outlier);
 
 
 right = bFP(matched);
 left = aFP(first_NN(matched));
 
-% draw two pic & matched feature
+%% output
+bFP_position = [right.x; right.y]';
+aFP_position = [left.x; left.y]';
+
+%% draw two pic & matched feature
+
 figure
 imshow([a b]);
 hold on;
 for i =1: length(right)
-    str = [' ' num2str(i)];
-    text(right(i).x+size(a,2), right(i).y, str);
-    plot (right(i).x+size(a,2), right(i).y, 'rx');
+    RF = [right(i).x+size(a,2), right(i).y];
+    LF = [left(i).x, left(i).y];
     
-    text(left(i).x, left(i).y, str);
-    plot (left(i).x, left(i).y, 'bx');
-end
+    %str = [' ' num2str(i)];
+    %text(right(i).x+size(a,2), right(i).y, str);
+    %text(left(i).x, left(i).y, str);
 
+    line ([LF(1) RF(1)], [LF(2) RF(2)], 'color', [0 1 0]);
+    
+    plot (RF(1), RF(2), 'r<');
+    plot (LF(1), LF(2), 'b>');
+end
+%}
 end
 
